52a4b6130b7acad3d51a3386ffaa8275ab9f26cd
bravomikekilo <bmk1221@126.com>
2017-04-02 12:58:53 +0800

    remove old tensorflow_fs

--- tensorflow_fs/build.sh
+++
@@ -1,7 +0,0 @@
-bazel build -c opt //tensorflow/python:pywrap_tensorflow_fs
-cd ..
-rm mxconsole/lib/native/pywrap*
-rm mxconsole/lib/native/_pywrap*
-cp  tensorflow_fs/bazel-bin/tensorflow/python/pywrap_tensorflow_fs.py \
-    tensorflow_fs/bazel-bin/tensorflow/python/_pywrap_tensorflow_fs.so \
-    mxconsole/lib/native/
