1743c93368cf57860ad1dd0946cc76532b7b23b1
bravomikekilo <bmk1221@126.com>
2017-04-02 13:49:33 +0800

    fix the update_native.sh

--- update_native.sh
+++ update_native.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 # script used for update the tensorflow_fs extension
 cd tensorflow_fs
-bazel build -c opt //tensorflow/python:pywrap_tensorflow_fs
+./build.sh
 cd ..
 echo "removing old native library"
 rm mxconsole/lib/native/pywrap*
 rm mxconsole/lib/native/_pywrap*
 echo "clean finish"
-cp tensorflow_fs/bazel-bin/tensorflow/python/pywrap_tensorflow_fs.py \
-   tensorflow_fs/bazel-bin/tensorflow/python/_pywrap_tensorflow_fs.so \
+cp tensorflow_fs/bin/pywrap_tensorflow_fs.py \
+   tensorflow_fs/bin/_pywrap_tensorflow_fs.so \
    mxconsole/lib/native/
 echo "done"
